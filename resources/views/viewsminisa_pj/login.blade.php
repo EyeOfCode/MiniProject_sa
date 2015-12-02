116dbcfc0ba76045ff73ee16bbb43f95a6c84047
zhansaki <zhansaki@gmail.com>
2015-12-02 17:47:26 +0700

    Zzzzzzz
    olo

--- resources/views/viewsminisa_pj/login.blade.php
+++ resources/views/viewsminisa_pj/login.blade.php
@@ -10,7 +10,7 @@
 	@section('login')
 	<center>
 		<table border="0">
-			<tr><tr><td colspan="2"><center><h3>Login</h3></center></td><br><br>
+			<tr><tr><td colspan="2"><center><h3>Login555555555555555555555</h3></center></td><br><br>
 				<form method="post" action="./Cklogin" name="login">
 					<?php echo csrf_field(); ?>
 					<tr><td>User Name: <br><br></td><td><input type="text" name="username"><br><br></td></tr>
